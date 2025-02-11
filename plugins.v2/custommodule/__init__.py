268a76ab09aecf6ab6e2744c327e388c010e9ea3
LiYuanXie <14929482+liyuanxie@user.noreply.gitee.com>
2025-02-11 16:06:29 +0800

    添加插件

--- plugins.v2/custommodule/__init__.py
+++ plugins.v2/custommodule/__init__.py
@@ -16,7 +16,7 @@ class CustomModule(_PluginBase):
     # 插件名称
     plugin_name = "自定义模块"
     # 插件描述
-    plugin_desc = "加载自定义模块。"
+    plugin_desc = "修改过滤器模块,支持50%和2x。"
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/InfinityPacer/MoviePilot-Plugins/main/icons/customplugin.png"
     # 插件版本
